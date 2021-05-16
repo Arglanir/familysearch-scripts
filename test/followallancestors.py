a23731bd4808be7cf42c1f403d9fad8a35d88346
Cedric <arglanir+github@gmail.com>
2021-05-16 09:46:16 +0200

    Better handling when following all. Allow following multiple parents.

--- test/followallancestors.py
+++ test/followallancestors.py
@@ -4,6 +4,7 @@ Does not go inside the children and theirs spouses."""
 from testbase import getFamilySearchDriver, selectThroughSR, goTo
 import time, datetime
 
+
 def main():
     done = set()
     clicked = list()
@@ -16,7 +17,8 @@ def main():
         identifier = url.split("/")[-1]
         todo.append(identifier)
         betweengeneration = 1
-        while todo:
+        try:
+          while todo:
             identifier = todo.pop(0)
             if isinstance(identifier, int):
                 print("Starting generation", identifier, "(in previous:", betweengeneration, ")")
@@ -29,6 +31,7 @@ def main():
             print(datetime.datetime.now()-start, "- Doing", identifier, len(done), "done, ", len(todo)+1, "remaining")
             goTo(driver, identifier)
             time.sleep(3)
+            # waiting for name
             while True:
                 try:
                     nameel = selectThroughSR(driver, "fs-person-page",
@@ -47,38 +50,50 @@ def main():
             print(" ", nameel.text)
             try:
                 elwatch = selectThroughSR(driver, "fs-person-page", "fs-watch", "button")
-                if "Regarder" in elwatch.text:
+                if elwatch.text == "":
+                    print("Waiting a little for the button to appear")
+                    time.sleep(10)
+                    elwatch = selectThroughSR(driver, "fs-person-page", "fs-watch", "button")
+                if "Suivre" in elwatch.text:
                     elwatch.click()
                     time.sleep(1)
                     clicked.append(identifier)
                     print("  clicked!")
+                elif "Abonnements" not in elwatch.text:
+                    print("Impossible to detect 'Following state': " + repr(elwatch.text))
+                    return
             except:
-                print("  no button")
+                print("  no button - probably living")
             done.add(identifier)
 
             # finding parents
-            for i in range(1,3):
-                try:
-                    parentel = selectThroughSR(driver, "fs-person-page",
-                                   "fs-tree-person-details", 
-                                   "fs-tree-person-family", 
-                                   "fs-family-members", 
-                                   "ul#parents",
-                                   "fs-family-members-block",
-                                   "fs-family-members-couple",
-                                   "#spouse%s-display" % i)
-                    ident = parentel.get_attribute("destination").split("/")[-1]
-                    if ident not in done and ident not in todo:
-                        todo.append(ident)
-                    else:
-                        print("  Found already found parent:", ident)
-                except:
-                    print("  no parent", i)
-        end = datetime.datetime.now()
-        print("Total time:", end - start)
-        print("Total followed:", len(clicked), len(done))
+            liels = selectThroughSR(driver, "fs-person-page",
+                                   "fs-tree-person-details",
+                                   "fs-tree-person-family",
+                                   "fs-family-members",
+                                   "ul#parents > li", allLast=True)
+            for j, li in enumerate(liels):
+                for i in range(1, 3):
+                    try:
+                        parentel = selectThroughSR(driver,
+                                                   "fs-family-members-block",
+                                                   "fs-family-members-couple",
+                                                   "#spouse%s-display" % i,
+                                                   origin=li)
+                        ident = parentel.get_attribute("destination").split("/")[-1]
+                        if ident not in done and ident not in todo:
+                            todo.append(ident)
+                        else:
+                            print("  Found already found parent:", ident)
+                    except KeyboardInterrupt:
+                        raise
+                    except:
+                        print("  no parent", j, i)
+        finally:
+            end = datetime.datetime.now()
+            print("Total time:", end - start)
+            print("Total followed: ", len(clicked), "/", len(done), sep="")
             
-        
 
 if __name__ == "__main__":
     main()
